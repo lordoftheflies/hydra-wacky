a7a5c065f3308522fc2c9ab9ed9f57624e8880bb
lordoftheflies <heglas11@gmail.com>
2016-02-17 20:40:20 +0100

    Storage tier info dto.

---
+++ hydra-common/src/main/java/com/ge/current/innovation/StorageTierInfoDto.java
@@ -0,0 +1,15 @@
+package com.ge.current.innovation;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lordoftheflies
+ */
+public class StorageTierInfoDto {
+    
+}
