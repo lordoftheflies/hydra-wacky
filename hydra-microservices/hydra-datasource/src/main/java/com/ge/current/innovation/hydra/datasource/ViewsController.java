012cefabcf9768e0f051cbe97050747cedc90104
lordoftheflies <heglas11@gmail.com>
2016-02-17 20:40:05 +0100

    Add swagger.

---
+++ hydra-microservices/hydra-datasource/src/main/java/com/ge/current/innovation/hydra/datasource/ViewsController.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ge.current.innovation.hydra.datasource;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author lordoftheflies
+ */
+@RestController
+public class ViewsController {
+    
+}
--- hydra-microservices/hydra-datasource/src/main/java/com/ge/current/innovation/hydra/datasource/VisualizationController.java
+++ hydra-microservices/hydra-datasource/src/main/java/com/ge/current/innovation/hydra/datasource/VisualizationController.java
@@ -6,19 +6,19 @@
 package com.ge.current.innovation.hydra.datasource;
 
 import com.ge.current.innovation.DataFilter;
+import com.ge.current.innovation.StorageTierInfoDto;
 import com.ge.current.innovation.HeatMapDto;
 import com.ge.current.innovation.KpiDto;
 import com.ge.current.innovation.SerieDto;
 import com.ge.current.innovation.hydra.storage.entities.DataPointEntity;
 import com.ge.current.innovation.hydra.storage.message.dal.DataPointRepository;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +28,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @author lordoftheflies
  */
 @RestController
-@RequestMapping(path = "/visialization", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(
+        path = "/visialization", 
+        consumes = MediaType.APPLICATION_JSON_VALUE, 
+        produces = MediaType.APPLICATION_JSON_VALUE)
 public class VisualizationController {
 
     private static final Logger LOG = Logger.getLogger(VisualizationController.class.getName());
@@ -36,10 +39,16 @@ public class VisualizationController {
     @Autowired
     private DataPointRepository dataPointRepo;
 
+    @RequestMapping(method = RequestMethod.GET)
+    public StorageTierInfoDto info(@RequestBody DataFilter filter) {
+        LOG.log(Level.INFO, "Storage tier info ...");
+        return new StorageTierInfoDto();
+    }
+    
     @RequestMapping(
-            consumes = "/set",
-            method = RequestMethod.GET)
-    public List<KpiDto> dataSet(DataFilter filter) {
+            path = "/set",
+            method = RequestMethod.POST)
+    public List<KpiDto> dataSet(@RequestBody DataFilter filter) {
         LOG.log(Level.INFO, "Visualize data (filter: {0}) ...", filter);
         List<KpiDto> dto = dataPointRepo.findAll().stream()
                 .map((DataPointEntity entity) -> new KpiDto("assetMeterUri", "name", entity.getTimeStamp(), entity.getVal()))
@@ -49,7 +58,10 @@ public class VisualizationController {
     }
 
     
-    public HeatMapDto heatMap(DataFilter filter) {
+    @RequestMapping(
+            path = "/heatmap",
+            method = RequestMethod.POST)
+    public HeatMapDto heatMap(@RequestBody DataFilter filter) {
         LOG.log(Level.INFO, "Visualize data (filter: {0}) ...", filter);
         HeatMapDto dto = new HeatMapDto();
         dto.setPoints(dataPointRepo.findAll().stream()
@@ -59,7 +71,10 @@ public class VisualizationController {
         return dto;
     }
 
-    public SerieDto seriesDto(DataFilter filter) {
+    @RequestMapping(
+            path = "/serie",
+            method = RequestMethod.POST)
+    public SerieDto seriesDto(@RequestBody DataFilter filter) {
         LOG.log(Level.INFO, "Visualize data (filter: {0}) ...", filter);
         SerieDto dto = new SerieDto();
         LOG.log(Level.INFO, "Result: {0}", filter);
