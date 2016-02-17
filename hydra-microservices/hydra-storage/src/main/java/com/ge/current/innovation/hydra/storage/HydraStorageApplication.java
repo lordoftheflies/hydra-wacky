8740e6e4da1a9acc11e8cd10c7e9ed2e916fb2f5
lordoftheflies <heglas11@gmail.com>
2016-02-17 13:58:23 +0100

    Create entities of the persistence layer.

---
+++ hydra-microservices/hydra-storage/src/main/java/com/ge/current/innovation/hydra/storage/HydraStorageApplication.java
@@ -0,0 +1,12 @@
+package com.ge.current.innovation.hydra.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydraStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HydraStorageApplication.class, args);
+    }
+}
---
+++ hydra-microservices/hydra-storage/src/main/java/com/ge/current/innovation/hydra/storage/MessageStoreLocalConfiguration.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ge.current.innovation.hydra.storage;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+/**
+ *
+ * @author lordoftheflies
+ */
+@Configuration
+public class MessageStoreLocalConfiguration {
+
+    @Value("${storage.timeseries.host}")
+    private String host;
+
+    @Value("${storage.timeseries.port}")
+    private String port;
+
+    @Value("${storage.timeseries.databaseName}")
+    private String databaseName;
+
+    @Value("${storage.timeseries.login}")
+    private String login;
+
+    @Value("${storage.timeseries.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource driver = new DriverManagerDataSource();
+        driver.setDriverClassName("org.postgresql.Driver");
+        driver.setUrl("jdbc:postgresql://" + host + ":" + port + "/" + databaseName);
+        driver.setUsername(login);
+        driver.setPassword(password);
+        return driver;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setDatabase(Database.POSTGRESQL);
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan(getClass().getPackage().getName());
+        factory.setDataSource(dataSource());
+
+        return factory;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return txManager;
+    }
+}
