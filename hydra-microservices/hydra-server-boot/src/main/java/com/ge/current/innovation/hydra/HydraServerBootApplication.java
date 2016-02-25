7601100196e93b827839147996dca7d26071bb44
lordoftheflies <heglas11@gmail.com>
2016-02-25 20:53:03 +0100

    Travis: CI

--- hydra-microservices/hydra-server-boot/src/main/java/com/ge/current/innovation/hydra/HydraServerBootApplication.java
+++ hydra-microservices/hydra-server-boot/src/main/java/com/ge/current/innovation/hydra/HydraServerBootApplication.java
@@ -3,9 +3,11 @@ package com.ge.current.innovation.hydra;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.ge.current.innovation.storage.jpa")
+@PropertySource("classpath:application-default.properties")
 public class HydraServerBootApplication {
 
     
